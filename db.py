903379cdf8256da31c4aacf72a8248ddcfa712a6
robbygozzarder <jdysmcl@tutanota.com>
2023-06-01 21:54:04 +0100

    .

--- db.py
+++ db.py
@@ -23,31 +23,3 @@ class Episode(Base):
 # Create an engine and connect to the database
 engine = create_engine('sqlite:///podcasts.db')
 Base.metadata.create_all(engine)
-
-# from sqlalchemy.orm import sessionmaker
-# from time import time
-# Session = sessionmaker(bind=engine)
-# session = Session()
-
-# # add podcasts
-# session.add(Podcast(
-#     name = 'Lanterne Rouge',
-#     rss_feed = 'https://anchor.fm/s/1311c8b8/podcast/rss'
-# ))
-# session.commit()
-
-# # Create posts
-# session.add(Episode(
-#     title = 'podcast',
-#     time = time(),
-#     content_url = 'https://pickles.com/content.mp3',
-#     podcast = Podcast.query.filter_by(name = podcast).first().id
-# ))
-# session.commit()
-
-# # Retrieve user's posts
-# podcast = session.query(Podcast).filter_by(name='Lanterne Rouge').first()
-# print(podcast.name)
-# print("Posts:")
-# for episode in podcast.episodes:
-#     print(f"Title: {episode.title}, Content: {episode.content_url}")
