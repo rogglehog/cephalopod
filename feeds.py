877db81174b820a59d0eb321649eca38943457ec
robbygozzarder <jdysmcl@tutanota.com>
2023-06-17 19:27:54 +0100

    .

--- feeds.py
+++ feeds.py
@@ -1,7 +1,7 @@
 from db import Podcast, Episode
 
 from feedparser import parse
-from time import time, mktime
+from time import time, mktime, strftime, localtime
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
@@ -46,7 +46,31 @@ def parse_feeds():
                 session.commit()
                 print(episode.title, 'from', podcast.name)
 
-parse_feeds()
+
+# query db for episodes
+# filter for age (in days)
+# orde by age
+# print info
+
+def sort_by_age(x):
+    return x.time
+    
+def pretty_list(age):
+    db_eps = session.query(Episode)
+    approved_eps = []
+    for ep in db_eps:
+        if ep.time > (time() - (age * 86400)):
+            approved_eps.append(ep)
+    approved_eps.sort(key=sort_by_age,reverse=True)
+            
+    for ep in approved_eps:
+        title = ep.title
+        date = strftime('%d/%m/%Y', localtime(ep.time))
+        podcast = ep.podcast.name
+        print(f'{title} ~ {podcast} ~ {date}')
+
+# parse_feeds()
+pretty_list(7)
 
 
 
